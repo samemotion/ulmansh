50deb0ed200406a011c91322f25c80e3ced7a7c8
samemotion <samemotion@users.noreply.github.com>
2019-04-23 03:09:41 +0000

    [ADD] stock_operation_type, picking

--- stock_sunat_reports/models/.ipynb_checkpoints/stock_picking-checkpoint.py
+++ stock_sunat_reports/models/.ipynb_checkpoints/stock_picking-checkpoint.py
@@ -5,3 +5,4 @@ class Picking(models.Model):
     _inherit = 'stock.picking'
 
     x_document_type = fields.Many2one('invoice.type',string='Tipo de comprobante de pago' , help='Document Number of the document related to the stock move. Invoice, External Move Document or internal move Document, in that order.')
+    x_operation_type = fields.Many2one('stock.operation.type',string='Operation Type')
